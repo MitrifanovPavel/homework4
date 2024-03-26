1d82d8e39664668cba2e0baef419eb7b8a359b14
MitrifanovPavel <163590983+MitrifanovPavel@users.noreply.github.com>
2024-03-26 18:15:23 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+immutable_var = [1,2], 'element', True
+print(type(immutable_var))
+#immutable_var[0] = 3
+#print(immutable_var) - неизменяемый тип данных
+
+mutable_list = [11, 24,87, 'zsljgjhcmvlxbhjm', 'lzlhgviulthMLinsovh']
+mutable_list[0] = 84358527
+print(mutable_list)
